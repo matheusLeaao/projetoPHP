9b09dffee81c351c3f34d2815306049d031b6fda
matheusLeaao <mathegiov@hotmail.com>
2019-11-20 15:35:22 -0300

    index

--- index.php
+++ index.php
@@ -1,47 +1,24 @@
 <?php
 	session_start();
-	$usuario = $_SESSION['usuario'];
+	$usuario=$_SESSION['usuario'];
 ?>
-<!DOCTYPE html>
+<!DOCYTYPE html>
 <html>
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
-    <script src="js/bootstrap.js" type="text/javascript"></script>
-    <head>
-	<title>Página Principal</title>
-	
-
-        <?php include 'header.php'; ?>
-	</head>
-
-    <br>
-    <br>
-   <body>
+<head>
+	<title>Pagina Principal</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<?php
+		if($usuario['perfil']!="adm"){
+			echo 'Você não ter permissao!';
+		}else{
+	?>
+	<h3>Seja bem viado, <?php print($usuario['login'])?></h3>
+	<p>Voce tem perfil: <?php print ($usuario['perfil']);?></p>
 
 	<?php
-		if($usuario['perfil'] !="adm"){
-			echo 'Você não tem permissao de acesso!';
-		}else{
-	>?
-	<h3>Seja bem viado, <?php print($usuario['login'])?></h3>
-	<p>Você tem perfil de acesso: <?php print ($usuario['perfil']);?></p>
-	
-    <form action ='cadastroAgenda.php'Method='post'>
-        <div class="form-group">
-        <fieldset>
-            <legend>FORMULARIO DE CADASTRO</legend>
-            <BR>
-            <LABEL>Nome: </LABEL><input type ='text'name="nome" placeholder="insira seu nome aqui" class="form-control">
-            <br><br>
-            <LABEL>Telefone: </LABEL><input type ='number'name="telefone" placeholder="insira seu telefone aqui" class="form-control">
-            <br><br>
-            <input type ='submit'name="btnCadastrar"class="btn btn-primary btn-block">
-        </fieldset>
-            </div>
-    </form>
-    
-    <footer>
-        
-         <?php include 'footer.php'; ?>
-    </footer>
-    </body>
+		}
+	?>
+</body>
 </html>
---
+++ temp(index).php
@@ -0,0 +1,47 @@
+<?php
+	session_start();
+	$usuario = $_SESSION['usuario'];
+?>
+<!DOCTYPE html>
+<html>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+    <script src="js/bootstrap.js" type="text/javascript"></script>
+    <head>
+	<title>Página Principal</title>
+	
+
+        <?php include 'header.php'; ?>
+	</head>
+
+    <br>
+    <br>
+   <body>
+
+	<?php
+		if($usuario['perfil'] !="adm"){
+			echo 'Você não tem permissao de acesso!';
+		}else{
+	>?
+	<h3>Seja bem viado, <?php print($usuario['login'])?></h3>
+	<p>Você tem perfil de acesso: <?php print ($usuario['perfil']);?></p>
+	
+    <form action ='cadastroAgenda.php'Method='post'>
+        <div class="form-group">
+        <fieldset>
+            <legend>FORMULARIO DE CADASTRO</legend>
+            <BR>
+            <LABEL>Nome: </LABEL><input type ='text'name="nome" placeholder="insira seu nome aqui" class="form-control">
+            <br><br>
+            <LABEL>Telefone: </LABEL><input type ='number'name="telefone" placeholder="insira seu telefone aqui" class="form-control">
+            <br><br>
+            <input type ='submit'name="btnCadastrar"class="btn btn-primary btn-block">
+        </fieldset>
+            </div>
+    </form>
+    
+    <footer>
+        
+         <?php include 'footer.php'; ?>
+    </footer>
+    </body>
+</html>
